bbb267cf6ad692080b31b006cdee81a0b868019a
Kushagra Chandak <kushagrachandak06@gmail.com>
2021-04-15 18:55:35 +0800

    Extended to >2 modules

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from grid import GridEnv
 from gridAgents import PolMixAgent, QModule, PolMixModule
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from columnar import columnar
 
 
 def run(agent, n_epi=500, max_steps=500, learn=True):
@@ -34,7 +35,7 @@ def run(agent, n_epi=500, max_steps=500, learn=True):
           
         agent.eps *= 0.99
         returns.append(cumulative_r)
-        print(path)
+        # print(path)
     return returns
 
 
@@ -85,20 +86,20 @@ def get_greedy_pi(q):
     
     return pi
 
-def get_pi(q):
-    pi = []
-    for s in range(env1.observation_space.n):
-        pi_s = softmax(q[s])
-        pi.append(pi_s)
-
-    return np.asarray(pi)
+def get_pi(q_list, total_modules):
+    pi_list = []
+    for m in range(total_modules):
+        pi = []
+        for s in range(env1.observation_space.n):
+            pi_s = softmax(q_list[m][s])
+            pi.append(pi_s)
+        pi = np.asarray(pi)
+        pi_list.append(pi)
+    
+    return np.asarray(pi_list)
 
 def sample_pi(pi, cur_state):
-    # new_pi = np.empty((env1.observation_space.n, env1.action_space.n))
-    # for s in range(env1.observation_space.n):
-    #     new_pi[s] = pi[s]/np.sum(pi[s])
-    new_pi = pi
-    return new_pi, np.random.choice(4, p=new_pi[cur_state])
+    return pi, np.random.choice(4, p=pi[cur_state])
 
 def get_occupancy_measure(pi_k, gamma=0.9):
 	total_states = env1.observation_space.n
@@ -127,45 +128,51 @@ def get_occupancy_measure(pi_k, gamma=0.9):
 
 def get_constraints_matrix(total_vars, total_constraints):
     A = np.zeros((total_constraints, total_vars))
-    j = 0
     for i in range(total_constraints):
-        A[i][j] = 1
-        A[i][j+total_constraints] = 1
-        j += 1
+        for m in range(total_modules):
+            A[i][i+m*total_constraints]
     
     return A
 
 def coeff_constr(coeff):
     total_constraints = env1.observation_space.n 
-    total_vars = env1.observation_space.n * 2
+    total_vars = env1.observation_space.n * total_modules
     b = np.ones(total_constraints)
     A = get_constraints_matrix(total_vars, total_constraints)
     return (A.dot(coeff)-b)
 
-def arb_loss(coeff, d_sa, q_k, pi1_k, pi2_k):
+def arb_loss(coeff, dsa_k, q_k, pi_array):
     total_states = env1.observation_space.n
     total_actions = env1.action_space.n
     loss = 0.
+    coeff_s = np.zeros(total_modules)
     for s in range(total_states):
-        coeff1_s, coeff2_s = coeff[s], coeff[s+total_states]
+        
+        i = 0
+        for m in range(total_modules):
+            coeff_s[m] = coeff[s+i]
+            i += total_states
+
         for a in range(total_actions):
-            pi_arb = coeff1_s*pi1_k[s,a] + coeff2_s*pi2_k[s,a]
+            pi_arb = 0.
+            for m in range(total_modules):
+                pi_arb += coeff_s[m] * pi_array[m,s,a]
             logpi_arb = 0 if (pi_arb<=0 or np.isinf(pi_arb) or np.isnan(pi_arb)) else np.log(pi_arb)
-            loss += d_sa[s,a] * q_k[s,a] * logpi_arb
+            loss += dsa_k[s,a] * q_k[s,a] * logpi_arb
     
     return -loss
 
 
-def solve_coeff(coeff1, coeff2, q_k, dsa_k, pi1_k, pi2_k):
-    total_variables = env1.observation_space.n * 2
+def solve_coeff(coeff, q_k, dsa_k, pi_array):
+    total_variables = env1.observation_space.n * total_modules
     constr = {'type':'eq', 'fun':coeff_constr}
     bnds = [(0, 1+1e-8)] * total_variables
 
-    f0 = np.concatenate((coeff1,coeff2))
+    f0 = np.concatenate(coeff)
 
-    res = optimize.minimize(arb_loss, x0=f0, args=(dsa_k, q_k, pi1_k, pi2_k), method='SLSQP', constraints=constr, bounds=bnds, options={'disp':False})
-    obj_val = arb_loss(res.x, dsa_k, q_k, pi1_k, pi2_k)
-    # print(obj_val)
+    res = optimize.minimize(arb_loss, x0=f0, args=(dsa_k, q_k, pi_array), method='SLSQP', constraints=constr, bounds=bnds, options={'disp':False})
+    # obj_val = arb_loss(res.x, dsa_k, q_k, pi_array)
+    
     return res.x
 
 def get_q(pi_k, gamma=0.9):
@@ -202,56 +209,60 @@ def get_q(pi_k, gamma=0.9):
     return x
 
 
-def optimize_pi(pi_k, pi1_k, pi2_k, coeff1, coeff2):
-    total_states = env1.observation_space.n
+def optimize_pi(pi_k, pi_array, coeff):
     q_k = get_q(pi_k)
     d_s, d_sa = get_occupancy_measure(pi_k)
-    coeff = np.concatenate((coeff1, coeff2))
-    obj_val = arb_loss(coeff, d_sa, q_k, pi1_k, pi2_k)
-    coeff = solve_coeff(coeff1, coeff2, q_k, d_sa, pi1_k, pi2_k)
+    coeff = solve_coeff(coeff, q_k, d_sa, pi_array)
     
-    return coeff[:total_states], coeff[total_states:]
+    return np.reshape(coeff, newshape=(total_modules, env1.observation_space.n))
 
 
-def test_tab_arb(q1, q2, n_epi=20, max_steps=500, learn=True):
+def get_init_coeff(total_modules):
+    coeff = np.full((total_modules, env1.observation_space.n,), fill_value=1./total_modules)
+    return coeff
+
+def test_tab_arb(q_list, n_epi=50, max_steps=500, learn=True):
     returns = []
-    coeff1, coeff2 = np.full(env1.observation_space.n, 0.5), np.full(env1.observation_space.n, 0.5)
-    pi1_k, pi2_k = get_greedy_pi(q1), get_greedy_pi(q2)
+    coeff = get_init_coeff(total_modules)
+    pi_array = get_pi(q_list, total_modules)
     for epi in range(n_epi):
         cumulative_r = 0
         step = 0
         epi_over = False
-        env1.reset()
-        env2.reset()
+        for m in range(total_modules):
+            env_list[m].reset()
+
+        pi_k = np.zeros((env1.observation_space.n, env1.action_space.n))
+        for i in range(total_modules):
+            pi_k += coeff[i][env1.cur_state] * pi_array[i]
+
         while (not epi_over) and (step < max_steps):
-            pi_k = coeff1[env1.cur_state] * pi1_k + coeff2[env1.cur_state] * pi2_k
-            pi_k, a_env = sample_pi(pi_k, env1.cur_state)
+            a_env = np.random.choice(4, p=pi_k[env1.cur_state]) # sample from pi_k
             s1, a1, s1_, r1, done1 = env1.step(a_env) # module1 and arb same
-            # s2, a2, s_2, r2, done2 = env2.step(a_env)
             if s1==s1_ and s1==env1.goal:
                 r1 = 10
             else:
                 r1 = 1
-            
-            if done1:
-                epi_over = True
-                # r1 = 10
 
             cumulative_r += r1
             step += 1
+
+            if done1:
+                epi_over = True
         
-        coeff1, coeff2 = optimize_pi(pi_k, pi1_k, pi2_k, coeff1, coeff2)
+        coeff = optimize_pi(pi_k, pi_array, coeff)
         
         # if epi%10 == 0:
-        print("Done: {}, cumulative_r: {}, coeff1: {}\n".format(epi, cumulative_r, coeff1))
+        print("Done: {}, cumulative_r: {}, coeff1: {}\n".format(epi, cumulative_r, coeff[0]))
         returns.append(cumulative_r)
 
-    np.save('coeff1_greedy', coeff1)
-    np.save('coeff2_greedy', coeff2)
+    # np.save('coeff1_greedy', coeff1)
+    # np.save('coeff2_greedy', coeff2)
     return returns
 
 
 def main():
+    np.set_printoptions(precision=4, suppress=True)
 
     # agent1 = QModule(env1)
     # agent2 = QModule(env2)
@@ -266,6 +277,23 @@ def main():
     # plt.plot(returns2)
     # plt.show()
 
+    # agent3 = QModule(env3)
+    # agent4 = QModule(env4)
+    # returns3, returns4 = run(agent3), run(agent4)
+    # np.save('m3_q3', agent3.q)
+    # np.save('m4_q4', agent4.q)
+    # plt.plot(returns3)
+    # plt.show()
+    # plt.plot(returns4)
+    # plt.show()
+    # q3, q4 = np.load('m3_q3.npy'), np.load('m4_q4.npy')
+    # pi3, pi4 = get_pi(q3), get_pi(q4)
+    # print("pi3")
+    # print(pi3)
+    # print("\n")
+    # print("pi4")
+    # print(pi4)
+
     # pol_mix_agent = PolMixAgent(env1, env2)
     # arb_agent = PolMixModule(env1)
     # arb_returns = test_arb(arb_agent)
@@ -273,36 +301,53 @@ def main():
 
     q1 = np.load('m1_q1.npy')
     q2 = np.load('m2_q2.npy')
-    tab_arb_returns = test_tab_arb(q1, q2)
+    q3 = np.load('m3_q3.npy')
+    q4 = np.load('m4_q4.npy')
+    tab_arb_returns = test_tab_arb([q1, q2, q3, q4])
     plt.plot(tab_arb_returns)
     plt.show()
-    pi1 = get_greedy_pi(q1)
-    pi2 = get_greedy_pi(q2)
-    coeff1 = np.load('coeff1_greedy.npy')
-    coeff2 = np.load('coeff2_greedy.npy')
+    # pi1 = get_pi(q1)
+    # pi2 = get_pi(q2)
+    # pi3 = get_pi(q3)
+    # pi4 = get_pi(q4)
+    
+    # coeff1 = np.load('coeff1.npy')
+    # coeff2 = np.load('coeff2.npy')
 
-    print("pi1")
-    print(pi1)
-    print("\n")
 
-    print("pi2")
-    print(pi2)
-    print("\n")
+    # # headers_pi = ["LEFT", "UP", "RIGHT", "DOWN"]
+    # # table_pi1 = columnar(pi1.tolist(), headers_pi, no_borders=True)
+    # # table_pi2 = columnar(pi2.tolist(), headers_pi, no_borders=True)
+    # # pi12 = np.hstack((pi1, pi2))
+    # # headers_pi12 = ["LEFT", "UP", "RIGHT", "DOWN", "LEFT", "UP", "RIGHT", "DOWN"]
+    # # table_pi12 = columnar(pi12.tolist(), headers_pi12)
+    # # print(table_pi12)
+    
+    # print("pi1")
+    # print(pi1)
+    # print("\n")
 
-    np.set_printoptions(precision=2, suppress=True)
-    pi_arb = np.empty((16, 4))
-    for s in range(16):
-        pi_arb[s] = coeff1[s]*pi1[s] + coeff2[s]*pi2[s]
-    print("pi_arb")
-    print(pi_arb)
+    # print("pi2")
+    # print(pi2)
+    # print("\n")
 
-    np.save('m1_pi_greedy', pi1)
-    np.save('m2_pi_greedy', pi2)
-    np.save('pi_arb_greedy', pi_arb)
+    # pi_arb = np.empty((16, 4))
+    # for s in range(16):
+    #     pi_arb[s] = coeff1[s]*pi1[s] + coeff2[s]*pi2[s]
+    # print("pi_arb")
+    # print(pi_arb)
+
+    # np.save('m1_pi_greedy', pi1)
+    # np.save('m2_pi_greedy', pi2)
+    # np.save('pi_arb_greedy', pi_arb)
 
 if __name__ == "__main__":
     env1 = GridEnv(goal=15)
     env2 = GridEnv(goal=12)
+    env3 = GridEnv(goal=3)
+    env4 = GridEnv(goal=9)
+    env_list = [env1, env2, env3, env4]
+    total_modules = len(env_list)
     main()
 
 
