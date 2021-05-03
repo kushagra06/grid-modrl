25e193cdb6b51142830be370b1501f786be709a9
Kushagra Chandak <kushagrachandak06@gmail.com>
2021-05-03 13:03:52 +0800

    DQN working

--- main_nn.py
+++ main_nn.py
@@ -1,48 +1,98 @@
 import numpy as np 
-import torch
 from itertools import count
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 from torch_gridagents import DQNAgent
 from grid import GridEnv
 from utils import ReplayMemory, Transition
 
-TARGET_UPDATE = 3
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+TARGET_UPDATE = 3
+BATCH_SIZE = 8
+GAMMA = 0.9
+
+memory = ReplayMemory(10000)
 
 def get_state_vector(s, s_dim=16):
     x = np.zeros(s_dim)
-    x[s] = 1. 
-    return x
+    x[s] = 1.
+    x = np.expand_dims(x, 0) 
+    return torch.FloatTensor(x)
 
-def run(env, agent, n_epi=50):
-    memory = ReplayMemory(10000)
+def optimize_model(agent, target_agent, optimizer):
+    if len(memory) < BATCH_SIZE:
+        return
+    
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+    state_batch = torch.cat(batch.state)#.unsqueeze(1)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = agent(state_batch).gather(dim=1, index=action_batch)
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_agent(non_final_next_states).max(1)[0].detach()
+
+    expected_state_action_values = reward_batch + GAMMA * next_state_values
+
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+def run(env, n_epi=50):
+    agent = DQNAgent(BATCH_SIZE).to(device)
+    target_agent = DQNAgent(BATCH_SIZE).to(device)
+    target_agent.load_state_dict(agent.state_dict())
+    target_agent.eval()
+    optimizer = optim.RMSprop(agent.parameters())
     epi_durations = []
+    returns = []
+    steps = 0
     for epi in range(n_epi):
         env.reset()
+        cumulative_r = 0.
         for t in count():
             state = get_state_vector(env.cur_state)
-            action = agent.get_action(state)
-            s, a, s_, r, done = env.step(action)
+            action = agent.get_action_epsgreedy(state, steps)
+            np_action = action.detach().cpu().numpy()[0][0] #convert tensor to np
+            s, a, s_, r, done = env.step(np_action)
+            cumulative_r += r
+            steps += 1
             reward = torch.tensor([r], device=device)
             next_state = get_state_vector(env.cur_state)
 
             memory.push(state, action, next_state, reward)
 
-            optimize_model()
+            optimize_model(agent, target_agent, optimizer)
 
             if done:
                 epi_durations.append(t+1)
                 break
-
+        
+        returns.append(cumulative_r)
         if epi % TARGET_UPDATE == 0:
-            target_net.load_state_dict(agent.state_dict())
+            print("epi {} over".format(epi))        
+            target_agent.load_state_dict(agent.state_dict())
 
-
+    return returns
 
 def main():
     env = GridEnv()
-    agent = DQNAgent()
-    run(env, agent)
+    returns = run(env)
+    plt.plot(returns)
+    plt.show()
     print('Done')
 
     
--- torch_gridagents.py
+++ torch_gridagents.py
@@ -1,13 +1,18 @@
+import math 
 import numpy as np 
+import random
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.nn.init as init
+# import torch.optim as optim
+# import torch.nn.functional as F
+# import torch.nn.init as init
 
 from typing import Tuple, Callable
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class RLNetwork(nn.Module):
     def __init__(self):
@@ -17,30 +22,38 @@ class RLNetwork(nn.Module):
     def forward(self, *x):
         return x
 
-    def train(self, loss, optimizer):
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    # def train(self, loss, optimizer):
+    #     optimizer.zero_grad()
+    #     loss.backward()
+    #     optimizer.step()
 
 
 class DQNAgent(RLNetwork):
-    def __init__(self, a_dim=4, s_dim=16):
+    def __init__(self, batch_size=8, a_dim=4, s_dim=16):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(s_dim+a_dim, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, 1),
-            nn.ReLU()
+            nn.Linear(s_dim, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, a_dim)
         )
     
-    def forward(self, states: torch.Tensor, actions: torch.Tensor):
-        x = torch.cat([states, actions])
-        q_sa = self.linear_relu_stack(x)
-        return q_sa
+    # Called with either one element to determine next action, or a batch during optimization.
+    def forward(self, state: torch.Tensor):
+        q_s = self.linear_relu_stack(state)
+        return q_s
     
-    def get_action(state: torch.Tensor):
-        pass
+    def get_action_epsgreedy(self, state: torch.Tensor, steps_done: int, a_dim=4):
+        sample = random.random()
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
+        if sample > eps_threshold:
+            with torch.no_grad():
+                return self.forward(state).argmax().view(1,1)
+        else:
+            return torch.tensor([[random.randrange(a_dim)]], device=device, dtype=torch.long)
+
+
+        
 
 
